e3d90b082aa95922d1c8bb7449876fc964bc5341
jnt677 <jnt677677@gmail.com>
2024-08-31 11:03:49 +0700

    Create porfoiliio.js

---
+++ porfoiliio.js
@@ -0,0 +1,2 @@
+<script async src="https://static.coinstats.app/widgets/insights-widget.js"></script>
+<coin-stats-insight-widget type="tradesToday" bg-color="#1C1B1B" text-color="#FFFFFF" chart-primary-color="#FFA959" chart-secondary-color="#E9BC48" button-color="#FFA959" border-color="rgba(255,255,255,0.15)" font="Roboto, Arial, Helvetica" coin-id="bitcoin" ></coin-stats-insight-widget>
